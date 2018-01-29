c8f81e0d694f8b1b530f56e3726a5b9576a8b548
mvdr <mathieu.vandriessche@gmail.com>
2018-01-30 00:47:18 +0100

    Display the header of the genres selector

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class GenresSelector extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <article>
+        <header>
+          <h1>What kind of music will you broadcast ?</h1>
+          <p>(Select from 1 to {this.props.MaximumNbOfSelections} genres maximum)</p>
+        </header>
+      </article>
+    );
+  }
+}
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
+  <GenresSelector MaximumNbOfSelections="5" />,
+  document.getElementById('root')
 );

3cf0b164a6824346699c69ce6e6d1eb46585237f
mvdr <mathieu.vandriessche@gmail.com>
2018-01-30 22:36:21 +0100

    Display the list of genres

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import musicGenres from '../src/mocks/music_genres'
 
 function Feedback(props) {
   if (props.nbOfSelections === 0) {
@@ -9,6 +10,35 @@ function Feedback(props) {
     return <p><strong>You can only select up to {props.maximumNbOfSelections} genres!</strong></p>;
   }
   return <p>{props.nbOfSelections} genre{props.nbOfSelections > 1 ? 's':''} selected</p>;
+}
+
+function Checkbox(props) {
+  return (
+    <label for={props.id}><input type="checkbox" id={props.id} name="genres" value={props.id} />{props.label}</label>
+  );
+}
+
+function ListItem(props) {
+  const item = props.item;
+  return (
+    <li>
+      <Checkbox id={props.id} label={item.Name} />
+      {Array.isArray(item.Genres) && item.Genres.length > 0 ? <List list={item.Genres} /> : ''}
+    </li>
+  );
+}
+
+function List(props) {
+  const list = props.list;
+  const listItems = list.map((item) => {
+    const id = item.ID || item.Name.replace(/\s/g,'');
+    return <ListItem key={id} id={id} item={item}/>
+  });
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
 }
 
 class GenresSelector extends React.Component {
@@ -27,12 +57,13 @@ class GenresSelector extends React.Component {
           <p>(Select from 1 to {this.props.maximumNbOfSelections} genres maximum)</p>
           <Feedback nbOfSelections={this.state.nbOfSelections} maximumNbOfSelections={this.props.maximumNbOfSelections} />
         </header>
+        <List list={this.props.genres} />
       </article>
     );
   }
 }
 
 ReactDOM.render(
-  <GenresSelector maximumNbOfSelections="5" />,
+  <GenresSelector genres={musicGenres} maximumNbOfSelections="5" />,
   document.getElementById('root')
 );

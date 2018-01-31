4c64aa463a58bfe2c6f14ec7303e3f747ecca3e0
mvdr <mathieu.vandriessche@gmail.com>
2018-02-01 00:29:58 +0100

    Add the feedback component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Feedback(props) {
+  if (props.nbOfSelections === 0) {
+    return <p><strong>You must select at least one genre!</strong></p>;
+  }
+  if (props.nbOfSelections > props.maximumNbOfSelections) {
+    return <p><strong>You can only select up to {props.maximumNbOfSelections} genres!</strong></p>;
+  }
+  return <p>{props.nbOfSelections} genre{props.nbOfSelections > 1 ? 's':''} selected</p>;
+}
+
 class GenresSelector extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      nbOfSelections: 0,
+    };
   }
 
   render() {
@@ -11,7 +24,8 @@ class GenresSelector extends React.Component {
       <article>
         <header>
           <h1>What kind of music will you broadcast ?</h1>
-          <p>(Select from 1 to {this.props.MaximumNbOfSelections} genres maximum)</p>
+          <p>(Select from 1 to {this.props.maximumNbOfSelections} genres maximum)</p>
+          <Feedback nbOfSelections={this.state.nbOfSelections} maximumNbOfSelections={this.props.maximumNbOfSelections} />
         </header>
       </article>
     );
@@ -19,6 +33,6 @@ class GenresSelector extends React.Component {
 }
 
 ReactDOM.render(
-  <GenresSelector MaximumNbOfSelections="5" />,
+  <GenresSelector maximumNbOfSelections="5" />,
   document.getElementById('root')
 );
